e53d215b217823691aec6d526bea82fb1bfbe3b7
Albert Panesse <albert.panesse@gmail.com>
2020-04-15 15:12:13 +0700

    CRUD menucateogires

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   get 'table/detail/:id', to: 'table#detail'
   post 'table/checkin', to: 'table#checkin'
   post 'table/checkout', to: 'table#checkout'
-	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post 'menu-category/create', to: 'menu_category#create'
+  put 'menu-category/update', to: 'menu_category#update'
+  delete 'menu-category/delete', to: 'menu_category#delete'
+  get 'menu-category/one/:id', to: 'menu_category#one'
+  get 'menu-category/many', to: 'menu_category#many'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_10_093043) do
+ActiveRecord::Schema.define(version: 2020_04_15_055521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,20 @@ ActiveRecord::Schema.define(version: 2020_04_10_093043) do
     t.integer "tablenum"
     t.string "custname"
     t.datetime "dtime"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "menu_categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "menus", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.string "tags"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
